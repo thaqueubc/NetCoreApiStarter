7c07feb35771de902322e89921628f67ea54b3aa
philweier <pweier@bcit.ca>
2020-01-26 22:53:04 -0800

    add controller methods

--- NetCoreApiStarter/Controllers/TodoController.cs
+++ NetCoreApiStarter/Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NetCoreApiStarter.Models;
 
 namespace NetCoreApiStarter.Controllers
 {
@@ -11,5 +12,82 @@ namespace NetCoreApiStarter.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var items = _context.ToDos.ToList();
+            if (items == null || items.Count <= 0)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(items);
+
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.ToDos.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ToDo todo)
+        {
+            try
+            {
+                _context.ToDos.Add(todo);
+                _context.SaveChanges();
+                // This allows for a status code of 201 (Created)
+                return new ObjectResult(todo);
+            }
+            catch (Exception e)
+            {
+                //Bad Request status code 400
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("MyEdit")] // Custom route
+        public IActionResult GetByParams([FromBody]ToDo todo)
+        {
+            var item = _context.ToDos.Where(t => t.Id == todo.Id).FirstOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                item.IsComplete = todo.IsComplete;
+                _context.SaveChanges();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpDelete]
+        [Route("MyDelete")] // Custom route
+        public IActionResult MyDelete(long id)
+        {
+            var item = _context.ToDos.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _context.ToDos.Remove(item);
+            _context.SaveChanges();
+            return new ObjectResult(item);
+        }
     }
 }

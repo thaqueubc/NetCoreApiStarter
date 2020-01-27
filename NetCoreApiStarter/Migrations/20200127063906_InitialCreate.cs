5701a1beed351977192d5d569cc9eb528273ad2a
philweier <pweier@bcit.ca>
2020-01-26 22:41:05 -0800

    add database

---
+++ NetCoreApiStarter/Migrations/20200127063906_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreApiStarter.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "CreatedOn", "Description", "IsComplete", "Priority" },
+                values: new object[] { 1, new DateTime(2020, 1, 26, 22, 39, 6, 675, DateTimeKind.Local).AddTicks(4031), "Clean house", false, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "CreatedOn", "Description", "IsComplete", "Priority" },
+                values: new object[] { 2, new DateTime(2020, 1, 26, 22, 39, 6, 677, DateTimeKind.Local).AddTicks(5236), "Bake cake", false, 3 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
---
+++ NetCoreApiStarter/Models/DbHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreApiStarter.Models
+{
+    public class ToDo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public int Priority { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDo>().HasData(
+                new { Id = 1, Description = "Clean house", IsComplete = false, Priority = 1, CreatedOn = DateTime.Now },
+                new { Id = 2, Description = "Bake cake", IsComplete = false, Priority = 3, CreatedOn = DateTime.Now }
+            );
+        }
+    }
+}
